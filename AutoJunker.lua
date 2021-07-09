fa0f5114cfb6c716afd5897f1dacfc86b85be63d
whonore <wolfhonore@gmail.com>
2021-07-09 13:53:05 -0400

    First working version

--- AutoJunker.lua
+++ AutoJunker.lua
@@ -1,29 +1,37 @@
 AutoJunker = {}
 AutoJunker.name = "AutoJunker"
 
-function AutoJunker:Initialize()
-end
-
-function AutoJunker.OnAddOnLoaded(event, addonName)
-    if addonName == AutoJunker.name then
-        AutoJunker:Initialize()
-    end
-
+local function init()
     EVENT_MANAGER:RegisterForEvent(AutoJunker.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, AutoJunker.OnInventoryChanged)
-    EVENT_MANAGER:AddFilterForEvent(AutoJunker.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_IS_NEW_ITEM, true)
     EVENT_MANAGER:AddFilterForEvent(AutoJunker.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_BAG_ID, BAG_BACKPACK)
     EVENT_MANAGER:AddFilterForEvent(AutoJunker.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_INVENTORY_UPDATE_REASON, INVENTORY_UPDATE_REASON_DEFAULT)
 end
 
-function AutoJunker.OnInventoryChanged(event, bagId, slotIdx, isNew, soundCategory, updateReason, stackCount)
-    if isNew and isJunk(bagId, slotIdx)
-        SetItemIsJunk(bagId, slotIdx, true)
+local function isJunk(bagId, slotIdx)
+    if not CanItemBeMarkedAsJunk(bagId, slotIdx) or IsItemJunk(bagId, slotIdx) then
+        return false
     end
+
+    -- TODO: Allow more types of filters
+    local type, _ = GetItemType(bagId, slotIdx)
+    return type == ITEMTYPE_TRASH
 end
 
-local function isJunk(bagId, slotIdx)
-    d("Checking " .. GetItemLink(bagId, slotIdx) .. " for junk")
-    return false and CanItemBeMarkedAsJunk(bagIdx, slotIdx)
+local function notifyJunk(bagId, slotIdx)
+    -- TODO: Use a nicer notification
+    d("Marking " .. GetItemLink(bagId, slotIdx) .. " as junk.")
+end
+
+function AutoJunker.OnAddOnLoaded(event, addon)
+    if addon ~= AutoJunker.name then return end
+    init()
+end
+
+function AutoJunker.OnInventoryChanged(event, bagId, slotIdx, isNew, soundCategory, updateReason, stackCount)
+    if stackCount > 0 and isJunk(bagId, slotIdx) then
+        notifyJunk(bagId, slotIdx)
+        SetItemIsJunk(bagId, slotIdx, true)
+    end
 end
 
 EVENT_MANAGER:RegisterForEvent(AutoJunker.name, EVENT_ADD_ON_LOADED, AutoJunker.OnAddOnLoaded)
